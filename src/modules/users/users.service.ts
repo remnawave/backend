a38c6d9cc673fb609a11f0b34e207c204899824d
TSLA <TSLA@LightHouse.com>
2025-09-29 08:30:23 +0300

    Revert "Fix: Debugging"
    This reverts commit 02ea80d2c583a163942e22d096fcffd2fcacde83.

--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -486,13 +486,6 @@ export class UsersService {
                 return { ...subscriptionUrlResult, response: undefined };
             }
 
-            this.logger.error(
-                'GET by uunic',
-                result.shortUuid,
-                result.username,
-                subscriptionUrlResult.response,
-            );
-
             return {
                 isOk: true,
                 response: { user: result, subscriptionUrl: subscriptionUrlResult.response },
