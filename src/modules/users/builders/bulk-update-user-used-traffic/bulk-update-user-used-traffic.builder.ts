2b3c417b60cee0e93e46f9a435925f31fd1474df
kastov <kastov@gog.sh>
2025-11-13 00:01:38 +0300

    refactor: SQL query for bulk user traffic updates

--- src/modules/users/builders/bulk-update-user-used-traffic/bulk-update-user-used-traffic.builder.ts
+++ src/modules/users/builders/bulk-update-user-used-traffic/bulk-update-user-used-traffic.builder.ts
@@ -18,34 +18,34 @@ export class BulkUpdateUserUsedTrafficBuilder {
         );
 
         return Prisma.sql`
-WITH data("inc_used","t_id","last_connected_node_uuid") AS (
-  VALUES ${values}
-),
-locked AS (
-  SELECT u."t_id"
-  FROM "user_traffic" u
-  JOIN data d ON d."t_id" = u."t_id"
-  ORDER BY u."t_id"
-  FOR UPDATE
-),
-updated_users AS (
-  UPDATE "user_traffic" AS u
-  SET
-    "used_traffic_bytes"          = u."used_traffic_bytes" + d."inc_used",
-    "lifetime_used_traffic_bytes" = u."lifetime_used_traffic_bytes" + d."inc_used",
-    "online_at"                   = NOW(),
-    "first_connected_at"          = COALESCE(u."first_connected_at", NOW()),
-    "last_connected_node_uuid"    = d."last_connected_node_uuid"
-  FROM data d
-  JOIN locked l ON l."t_id" = d."t_id"
-  WHERE d."t_id" = u."t_id"
-  RETURNING
-    u."t_id",
-    (u."first_connected_at" = u."online_at") AS "isFirstConnection"
-)
-SELECT "t_id" AS "tId"
-FROM updated_users
-WHERE "isFirstConnection";
-    `;
+        WITH data("inc_used","t_id","last_connected_node_uuid") AS (
+          VALUES ${values}
+        ),
+        locked AS (
+          SELECT u."t_id"
+          FROM data d
+          JOIN "user_traffic" u ON u."t_id" = d."t_id"
+          ORDER BY u."t_id"
+          FOR UPDATE OF u
+        ),
+        updated_users AS (
+          UPDATE "user_traffic" AS u
+          SET
+            "used_traffic_bytes"          = u."used_traffic_bytes" + d."inc_used",
+            "lifetime_used_traffic_bytes" = u."lifetime_used_traffic_bytes" + d."inc_used",
+            "online_at"                   = NOW(),
+            "first_connected_at"          = COALESCE(u."first_connected_at", NOW()),
+            "last_connected_node_uuid"    = d."last_connected_node_uuid"
+          FROM data d
+          JOIN locked l ON l."t_id" = d."t_id"
+          WHERE d."t_id" = u."t_id"
+          RETURNING
+            u."t_id",
+            (u."first_connected_at" = u."online_at") AS "isFirstConnection"
+        )
+        SELECT "t_id" AS "tId"
+        FROM updated_users
+        WHERE "isFirstConnection";
+        `;
     }
 }
