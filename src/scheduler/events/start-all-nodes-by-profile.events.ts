c10689adf205e4bbfd7aa5c2aa8bd8169be55e74
kastov <kastov@gog.sh>
2025-12-03 07:17:11 +0300

    fix: enhance deduplication handling

--- src/scheduler/events/start-all-nodes-by-profile.events.ts
+++ src/scheduler/events/start-all-nodes-by-profile.events.ts
@@ -10,24 +10,35 @@ import { QUEUES_NAMES } from '@queue/queue.enum';
 @Injectable()
 export class StartAllNodesByProfileQueueEvents implements OnModuleInit {
     private readonly logger = new Logger(StartAllNodesByProfileQueueEvents.name);
+    private waitingProfiles = new Set<string>();
 
     private queueEvents: QueueEvents;
 
     constructor(private readonly nodesQueuesService: NodesQueuesService) {}
 
     async onModuleInit() {
+        this.logger.log('Initializing deduplication event listener.');
+
         this.queueEvents = new QueueEvents(QUEUES_NAMES.NODES.START_ALL_BY_PROFILE, {
             connection: this.nodesQueuesService.queues.startAllNodesByProfile.opts.connection,
         });
 
         this.queueEvents.on('deduplicated', async (event) => {
             const { jobId, deduplicationId } = event;
+            const profileUuid = deduplicationId;
+
+            if (this.waitingProfiles.has(profileUuid)) {
+                this.logger.log(`[deduplicated] ${deduplicationId} – already waiting for retry`);
+                return;
+            }
+
+            this.waitingProfiles.add(profileUuid);
 
             this.logger.log(`[deduplicated] ${deduplicationId} – retrying job ${jobId} in 10s`);
 
-            const profileUuid = deduplicationId;
-
             await sleep(10_000);
+
+            this.waitingProfiles.delete(profileUuid);
 
             await this.nodesQueuesService.startAllNodesByProfile({
                 profileUuid,
