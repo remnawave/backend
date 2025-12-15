736dff699cd91874a5316501969554ff9de4c307
kastov <kastov@gog.sh>
2025-12-16 01:29:13 +0300

    –

--- prisma/migrations/20251215211455_subpage_tr/migration.sql
+++
@@ -1,2 +0,0 @@
--- Truncate all subscription page configs
-TRUNCATE TABLE "subscription_page_config" RESTART IDENTITY CASCADE;
---
+++ prisma/migrations/20251215222247_subpage_tr/migration.sql
@@ -0,0 +1,2 @@
+-- Remove all subscription page configs
+DELETE FROM "subscription_page_config";
