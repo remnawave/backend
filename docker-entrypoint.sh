0f8b639b6c5b194c1f81bf574dab1026d0efcb7c
kastov <yk@sent.com>
2026-07-31 22:45:23 +0300

    refactor: remove temporary warning from docker-entrypoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,33 +1,5 @@
 #!/bin/sh
 
-# TODO: remove later
-if [ "$I_UNDERSTAND_REST_API_BREAKING_CHANGES" != "true" ]; then
-    echo ""
-    echo "=================================================================="
-    echo "                         BREAKING CHANGES                        "
-    echo "                         Remnawave 3.0.0                          "
-    echo "=================================================================="
-    echo ""
-    echo "This dev-branch build contains BREAKING CHANGES in the REST API."
-    echo ""
-    echo "If you are NOT sure, stay on the previous dev-branch version by"
-    echo "pinning the image digest in your docker-compose.yml:"
-    echo ""
-    echo "  image: remnawave/backend@sha256:75b8f1d25b0d7754436e62df0e32f67e7592a6cb64dfe01d426d35e741032c59"
-    echo ""
-    echo "If you understand the consequences and want to continue anyway,"
-    echo "set the following environment variable:"
-    echo ""
-    echo "  I_UNDERSTAND_REST_API_BREAKING_CHANGES=true"
-    echo ""
-    echo "You can also deploy the latest dev-branch and try it out at:"
-    echo ""
-    echo "  https://try.rw"
-    echo ""
-    echo "=================================================================="
-    exit 1
-fi
-
 echo "Starting entrypoint script..."
 
 PRISMA="/opt/app/node_modules/.bin/prisma"
